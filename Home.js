35cb071430f645f78abdda309b97923449cdb0e2
Ninzato <61687110+Ninzato@users.noreply.github.com>
2022-06-17 01:08:51 +0800

    Update Home.js

--- Home.js
+++ Home.js
@@ -350,7 +350,7 @@ if (reversed == null) { reversed = false; }
 			createjs.Sound.play("swordsound");
 			
 			setTimeout(function(){
-			document.location.replace("http://127.0.0.1:8090/QuizMenu.html");
+			document.location.replace("https://ninzato.github.io/history-of-badr-war/QuizMenu.html");
 		    }, 1000);
 		}
 		
@@ -359,7 +359,7 @@ if (reversed == null) { reversed = false; }
 			createjs.Sound.play("swordsound");
 			
 			setTimeout(function(){
-			document.location.replace("http://127.0.0.1:8090/LessonMenu.html");
+			document.location.replace("https://ninzato.github.io/history-of-badr-war/LessonMenu.html");
 			}, 1000);
 		}
 		
@@ -369,7 +369,7 @@ if (reversed == null) { reversed = false; }
 			createjs.Sound.play("swordsound");
 			
 			setTimeout(function(){
-			document.location.replace("http://127.0.0.1:8090/CreditMenu.html");
+			document.location.replace("https://ninzato.github.io/history-of-badr-war/CreditMenu.html");
 			}, 1000);
 		}
 		
@@ -379,7 +379,7 @@ if (reversed == null) { reversed = false; }
 			createjs.Sound.play("doorsound");
 			
 			setTimeout(function(){
-			//document.location.replace("http://127.0.0.1:8090/z.html");
+			//document.location.replace("https://ninzato.github.io/history-of-badr-war/z.html");
 			window.close();
 			}, 1000);
 			
