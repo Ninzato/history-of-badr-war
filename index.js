ae4cf07454a314912640e7e38b7ec22c2c0a3b2c
aimanaress <102285518+aimanaress@users.noreply.github.com>
2022-06-17 19:03:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ if (reversed == null) { reversed = false; }
 			createjs.Sound.play("popsound");
 			
 			setTimeout(function(){
-			document.location.replace("/history-of-badr-war/Home.html");
+			document.location.replace("https://ninzato.github.io/Home.html");
 			}, 500);
 			
 		}
